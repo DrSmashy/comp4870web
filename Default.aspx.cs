e3c1cdf164ef5a67015e5f2f92a0ea8b278a5ff3
Johno Robson <johnathan.g.robson@gmail.com>
2017-01-20 13:57:07 -0800

    first commit

---
+++ Default.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        string path = MapPath("~/uploads");
+        string[] files = Directory.GetFiles(path);
+        ddlfiles.DataSource = files;
+        ddlfiles.DataBind();
+    }
+}
